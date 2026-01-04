e214d491129c1b763c2a69ad292f4fbcda8a654c
elblanquito <johanblancocata@gmail.com>
2026-01-04 02:40:04 -0500

    twitter follow

--- projects/Hola mundo/src/App.jsx
+++ projects/Hola mundo/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt="Johzuto" src="https://cdn.discordapp.com/avatars/1314995106335690879/f124941f86596c6ffe14646a0ec8389b.png?size=512"></img>
+                <div>
+                    <strong>Johzuto</strong>
+                    <span>@eljohzuto</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
 }
-
-export default App
